ee8b0a3529de9624577c7eb859789a90b7c2f9a7
kibet14isaac <117469732+kibet14isaac@users.noreply.github.com>
2025-03-17 22:29:53 +0300

    Add files via upload

---
+++ sidenav.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>ADMIN</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="assets/css/Material+Icons.css" />
+  <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+  <!-- Material Kit CSS -->
+  <link href="assets/css/material-dashboard.css?v=2.1.0" rel="stylesheet" />
+  <link href="assets/css/black-dashboard.css" rel="stylesheet" />
+  
+</head>
+
+<body class="dark-edition">
+  <div class="wrapper ">
+    <div class="sidebar" data-color="blue" data-background-color="green" data-image="./assets/img/download.jpeg">
+      
+      <div class="logo">
+        <a href="" class="simple-text logo-normal">
+          ADMIN
+        </a>
+      </div>
+      <div class="sidebar-wrapper ps-container ps-theme-default" data-ps-id="3a8db1f4-24d8-4dbf-85c9-4f5215c1b29a">
+        <ul class="nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">
+              <i class="material-icons"></i>
+              <p>DASHBOARD</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="adduser.php">
+              <i class="material-icons"></i>
+              <p>Add User</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="productlist.php">
+              <i class="material-icons"></i>
+              <p>PRODUCTS</p>
+            </a>
+            
+          </li>
+          
+          <li class="nav-item ">
+            <a class="nav-link" href="orders.php">
+              <i class="material-icons"></i>
+              <p>ORDERS</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="addproduct.php">
+              <i class="material-icons"></i>
+              <p>ADD PRODUCTS</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="print/print_pdf.php">
+              <i class="material-icons">
+              </i>
+              <p> PRINT REPORT</p>
+            </a>
+
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="manageuser.php">
+              <i class="material-icons"></i>
+              <p>USERS MANAGEMENT</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="index.php">
+              <i class="material-icons"></i>
+              <p>Logout</p>
+            </a>
+          </li>
+        
+        </ul>
+      </div>
+    </div>
+    
+    
